0d5283eb688761e963a9437911ec1457ab14e787
satoru <estrela.red.526@gmail.com>
2020-02-10 00:15:47 +0900

    --P.124, Next, start to p.125....

--- app.js
+++ app.js
@@ -1,31 +1,95 @@
 const http = require('http');
 const fs = require('fs');
-const indexHtmlFile = './index.html';
-const unicodeUTF8 = 'UTF-8';
+const ejs = require('ejs');
+const url = require('url');
+
+unicodeUTF8 = 'UTF-8';
+var iCount = 0;
+const index_page = fs.readFileSync('./index.ejs','utf8');
+const other_page = fs.readFileSync('./other.ejs','utf8');
+const style_css = fs.readFileSync('./style.css','utf8');
+
 var server = http.createServer(GetFromClient);
 
 server.listen(3000);
-console.log("Server Starts.");
-console.log("...");
+trace("Server Starts.");
+trace("...");
+
+// ここまでメインプログラム=================
 
 /**
  * 機能概要：createSreverの処理
- * @params {string} req
- * @params {string} res
+ * @param {?} req リクエスト
+ * @param {?} res レスポンス
  */
-function GetFromClient(req, res) {
-    var request = req;
-    var resonse = res;
-    fs.readFile(indexHtmlFile, unicodeUTF8, WriteToResponse);
+function GetFromClient(request, response) {
+    var url_parts = url.parse(request.url, true);
+    trace(url_parts);
+
+    switch (url_parts.pathname) {
+        case '/':
+            var query = url_parts.query;
+            var content = "test...";
+
+            if (query.msg != void 0) {
+                content += 'Your message is ' + query.msg + '.';
+            }
+            var content = ejs.render(index_page, {
+                title:"suzuki",
+                content:content,
+            });
+            response.writeHead(200, {'Content-Type': 'text/html'})
+            response.write(content);
+            response.end();
+            break;
+
+        case '/other':
+            var content = ejs.render(other_page, {
+                title:"other",
+                content:"new satoru",
+            });
+            response.writeHead(200, {'Content-Type': 'text/html'})
+            response.write(content);
+            response.end();
+            break;
+        
+        case '/style.css':
+            response.writeHead(200, {'Content-Type': 'text/css'})
+            response.write(style_css);
+            response.end();
+            break;
+        
+        deafult:
+            response.writeHead(200, {'Content-Type': 'text/plain'})
+            response.end('no page...');
+            break;
+    }
+    iCount++;
+    trace("Someone accesses me. [" + iCount + "番目のアクセスです]");
 }
 
+/**
+ * 機能概要：readFile完了後の処理
+ * @param {?} error [正常] -
+ *                  [異常] エラーに関する情報をまとめたオブジェクトを格納
+ * @param {?} data  ファイルから読み込んだデータ
+ */
 function WriteToResponse(error, data) {
-    response.setHeader('Content-Type','text.html');
     response.writeHead(200, {'Content-Type': 'text/html'});
     response.write(data);
-    response.end();    
+    response.end();
 }
 
+/**
+ * 機能概要：createSreverの処理
+ * @param {?} req リクエスト
+ * @param {?} res レスポンス
+ */
+function GetFromClient_01(req, res) {
+    request = req;
+    response = res;
+    fs.readFile('./index.html', 'UTF-8', WriteToResponse);
+}
 function TestArrayOfFor() {
     var array = new Array;
     var array2 = new Array;
@@ -58,3 +122,13 @@ var server1 = http.createServer(
         response.end(TestArrayOfFor());
     }
 );
+
+/** デバッグモードかどうか。本番公開時にはfalseにする */
+var DEBUG_MODE = true;
+
+/** デバッグモードでConsoleAPIが有効な場合にログを出力する */ 
+function trace(s) {
+    if (DEBUG_MODE && this.console && typeof console.log != "undefined") {
+        console.log(s);
+    }
+}
